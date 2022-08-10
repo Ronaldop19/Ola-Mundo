2a6c561779ed4f4a180b7c33fe860addbc5c5002
Ronaldo Amorim <ronaldop.amorim@hotmail.com>
2022-08-09 21:09:52 -0300

    olamundo.java
    Olá mundo em Java.

---
+++ OlaMundoJavaFX2/src/olamundojavafx2/label.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package olamundojavafx2;
+
+/**
+ *
+ * @author ronal
+ */
+class label {
+
+    static void setText(String hello_World) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+    
+}
